feb7762ce25cc18a567f9c6a4f37ed527e5a0e18
Casey Ruff <cruff@skagitcoop.org>
2020-12-14 16:32:05 -0800

    Updated model to include spawner_smolt and smolt_adult transitions
    Spawner_smolt transition is density independent and smolt_adult is density dependent due to estuarine life stage.

--- analysis/estimate_aggregate_ER.R
+++ analysis/estimate_aggregate_ER.R
@@ -8,20 +8,27 @@ if(!require("readr")) {
   library("readr")
 }
 
-install.packages("tidyverse")
-library("tidyverse")
-install.packages("magrittr")
-library("magrittr")
+if(!require("tidyverse")) {
+  install.packages("tidyverse")
+  library("tidyverse")
+}
+
+if(!require("magrittr")){
+  install.packages("magrittr")
+  library("magrittr")
+}
+
+
 
 datadir <- here("data")
 
-AEQ_harvest_SF <-readRDS(paste(datadir,"d_r62_tamm_lmtstkcmplt_no_m_nat_1992_2016.rds",sep = "/")) %>% 
-  select(stk_type:fisheryID, contains("Skagit SF")) %>% 
+AEQ_harvest <-readRDS(paste(datadir,"d_r62_tamm_lmtstkcmplt_no_m_nat_1992_2016.rds",sep = "/")) %>% 
+  select(stk_type:fisheryID, contains("Skagit")) %>% 
   mutate(tot_aeq = select(., contains("_aeq")) %>% rowSums(na.rm = T)) %>%
   filter(fisheryID != 74) %>% #catch only, leave out for total abundance
   group_by(year) %>% summarise(tot_aeq = sum(tot_aeq, na.rm = T))
 
 
-write_csv(AEQ_harvest_SF, file.path(datadir, "skagit_sf_chinook_aeq.csv"))
+write_csv(AEQ_harvest, file.path(datadir, "skagit_chinook_aeq_harvest.csv"))
 
 
